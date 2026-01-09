f052301bd2164f897f320c6fb5ec57439a2a2149
Ax-47 <armingLwnza@gmail.com>
2026-01-09 13:42:26 +0700

    add: input keyboard

--- src/systems/input.rs
+++ src/systems/input.rs
@@ -1,10 +1,25 @@
-use bevy::{app::AppExit, prelude::*};
+use bevy::prelude::*;
 use bevy_ratatui::event::KeyMessage;
 use ratatui::crossterm::event::KeyCode;
 pub fn input_system(mut messages: MessageReader<KeyMessage>, mut exit: MessageWriter<AppExit>) {
     for message in messages.read() {
-        if let KeyCode::Char('q') = message.code {
-            exit.write_default();
+        match message.code {
+            KeyCode::Char('w') => {
+                println!("w");
+            }
+            KeyCode::Char('a') => {
+                println!("a");
+            }
+            KeyCode::Char('s') => {
+                println!("s");
+            }
+            KeyCode::Char('d') => {
+                println!("d");
+            }
+            KeyCode::Esc => {
+                exit.write_default();
+            }
+            _ => {}
         }
     }
 }
