f991035e8b0fcf4f27b4ac49394a9837be181c1c
Ax-47 <armingLwnza@gmail.com>
2026-01-12 15:31:36 +0700

    add: overray

--- src/components/position.rs
+++ src/components/position.rs
@@ -10,10 +10,10 @@ impl Position {
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Self { x, y, z }
     }
-    pub fn incress(&mut self, x: f32, y: f32, z: f32) {
-        self.x += x;
-        self.y += y;
-        self.z += y;
+    pub fn incress(&mut self, dx: f32, dy: f32, dz: f32) {
+        self.x = (self.x + dx).clamp(0.0, 50.0);
+        self.y = (self.y + dy).clamp(0.0, 50.0);
+        self.z = (self.z + dz).clamp(0.0, 50.0);
     }
 
     pub fn get_position_as_u16(&self) -> (u16, u16, u16) {
--- src/systems/draw.rs
+++ src/systems/draw.rs
@@ -2,7 +2,7 @@ use crate::{components::position::Position, entities::Player};
 use bevy::prelude::*;
 use bevy_ratatui::RatatuiContext;
 use ratatui::layout::Rect as RRect;
-use ratatui::widgets::Paragraph;
+use ratatui::widgets::{Paragraph, Wrap};
 pub fn draw_system(
     mut context: ResMut<RatatuiContext>,
     query: Query<&Position, With<Player>>,
@@ -17,8 +17,18 @@ pub fn draw_system(
                 height: 1,
             };
 
-            let widget = Paragraph::new("O");
-            frame.render_widget(widget, area);
+            let play = Paragraph::new("O");
+            frame.render_widget(play, area);
+
+            let area = RRect {
+                x: 0,
+                y: 0,
+                width: 50,
+                height: 1,
+            };
+            let overray = Paragraph::new(format!("{:?}", pos.get_position_as_u16()))
+                .wrap(Wrap { trim: true });
+            frame.render_widget(overray, area);
         }
     })?;
 
